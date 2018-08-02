f0955385b67726fb5df626dfdb572afa36d25d7b
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-02 19:41:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 WIN_COMBINATIONS
-board = ["X","X","X",3,4,5,6,7,8,9]
+board = ["X",1,2,"X",4,5,"X",7,8,9]
 def won?(board)
     winning_combination=[]
     WIN_COMBINATIONS.each do |win_option|
